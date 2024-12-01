372e3b7acd204cf0b35534bc1b97dd4ab937d51c
lsrujan <srujanlanderi@gmail.com>
2024-12-01 10:52:13 +0530

    Api docs generation

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from flask import Flask
-from books_blueprint import books_bp
 from users_blueprint import users_bp
 
 app = Flask(__name__)
 
 # Register blueprints.
-app.register_blueprint(books_bp, url_prefix='/notebooks')
 app.register_blueprint(users_bp, url_prefix='/users')
 
 @app.route('/')
